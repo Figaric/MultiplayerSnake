6392573eb7af1e854637c707613c65d92e038799
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 01:12:35 +0300

    Add some validation stuff

---
+++ Source/Server/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Server.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TResponse : CQRSResponse, new()
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IValidator<TRequest> _validator;
+
+        public ValidationBehaviour() { }
+
+        public ValidationBehaviour(IValidator<TRequest> validator)
+        {
+            _validator = validator;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validator is null)
+                return await next();
+
+            var result = await _validator.ValidateAsync(request);
+
+            if (!result.IsValid)
+            {
+                new TResponse
+                {
+                    Ok = false,
+                    Errors = result.Errors
+                        .Select(e => new FieldError
+                        { 
+                            Field = e.PropertyName, 
+                            Message = e.ErrorMessage 
+                        })
+                        .ToList()
+                };
+            }
+
+            return await next();
+        }
+    }
+}
