470df9931cf28672fc5f9ddd5e6e563ac7530f02
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:23:38 +0300

    Database initial stuff

---
+++ Source/Server/Database/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiplayerSnake.Server.Entities;
+
+namespace MultiplayerSnake.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+    }
+}
