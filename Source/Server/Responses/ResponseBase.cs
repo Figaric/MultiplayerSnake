d16cb63a69cc98e5efacbadefa09b5617200056b
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:27:18 +0300

    Add ResponseBase

---
+++ Source/Server/Responses/ResponseBase.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MultiplayerSnake.Server.Responses
+{
+    public record ResponseBase
+    {
+        public HttpStatusCode StatusCode { get; init; } = HttpStatusCode.OK;
+
+        public IList<FieldError> Errors { get; init; }
+
+        public static ResponseBase Succeed => new ResponseBase();
+
+        public static ResponseBase Failed(params FieldError[] errors)
+            => new ResponseBase { StatusCode = HttpStatusCode.BadRequest, Errors = errors };
+    }
+
+    public record ResponseBase<TData> : ResponseBase
+    {
+        public TData Data { get; init; }
+
+        public static ResponseBase<TData> Succeed(TData data)
+            => new() { Data = data };
+    }
+}
