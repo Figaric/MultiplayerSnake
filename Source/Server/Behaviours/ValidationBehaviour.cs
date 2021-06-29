2900ff414719f3a51bebb9a8481f633885bd357b
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:22:58 +0300

    Add ValidationBehaviour

--- Source/Server/Behaviours/ValidationBehaviour.cs
+++ Source/Server/Behaviours/ValidationBehaviour.cs
@@ -1,13 +1,15 @@
 ﻿using FluentValidation;
 using MediatR;
+using MultiplayerSnake.Server.Responses;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultiplayerSnake.Server.Behaviours
 {
     public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-        where TResponse : CQRSResponse, new()
+        where TResponse : ResponseBase, new()
         where TRequest : IRequest<TResponse>
     {
         private readonly IValidator<TRequest> _validator;
@@ -26,20 +28,15 @@ namespace MultiplayerSnake.Server.Behaviours
 
             var result = await _validator.ValidateAsync(request);
 
-            if (!result.IsValid)
-            {
-                new TResponse
+            var fieldErrors = result.Errors
+                .Select(e => new FieldError
                 {
-                    Ok = false,
-                    Errors = result.Errors
-                        .Select(e => new FieldError
-                        { 
-                            Field = e.PropertyName, 
-                            Message = e.ErrorMessage 
-                        })
-                        .ToList()
-                };
-            }
+                    Field = e.PropertyName,
+                    Message = e.ErrorMessage
+                });
+
+            if (!result.IsValid)
+                return new TResponse { StatusCode = HttpStatusCode.BadRequest, Errors = fieldErrors.ToList() };
 
             return await next();
         }
