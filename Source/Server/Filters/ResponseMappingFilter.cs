83e15df49ab7422384d65154cca9b5bcc49ff07f
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:25:38 +0300

    Add ResponseMappingFilter
    A filter that converts ResponseBase to HttpResponse

---
+++ Source/Server/Filters/ResponseMappingFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MultiplayerSnake.Server.Responses;
+using System.Net;
+
+namespace MultiplayerSnake.Server
+{
+    public class ResponseMappingFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (
+                context.Result is ObjectResult objectResult &&
+                objectResult.Value is ResponseBase response &&
+                response.StatusCode is not HttpStatusCode.OK)
+
+                context.Result = new ObjectResult(response) { StatusCode = (int)response.StatusCode };
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+    }
+}
