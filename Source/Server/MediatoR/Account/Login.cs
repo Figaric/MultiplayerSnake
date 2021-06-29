bfaa56806fade6cea4c20d19ce60062ba634700e
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:28:41 +0300

    Add Login and Register methods using MediatoR and CQRS pattern

---
+++ Source/Server/MediatoR/Account/Login.cs
@@ -0,0 +1,55 @@
+﻿using Isopoh.Cryptography.Argon2;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MultiplayerSnake.Server.Responses;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+using MultiplayerSnake.Server.Dtos;
+using System.Linq;
+
+namespace MultiplayerSnake.Server
+{
+    public static class Login
+    {
+        public class Handler : IRequestHandler<UserLoginDto, ResponseBase>
+        {
+            private readonly ApplicationDbContext _context;
+            private readonly JwtManager _jwt;
+
+            public Handler(ApplicationDbContext context, JwtManager jwt)
+            {
+                _context = context;
+                _jwt = jwt;
+            }
+
+            public async Task<ResponseBase> Handle(UserLoginDto request, CancellationToken cancellationToken)
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username);
+
+                if (user == null)
+                {
+                    return ResponseBase.Failed(new FieldError
+                    {
+                        Field = nameof(request.Username),
+                        Message = "User with this username doesn't exist."
+                    });
+                }
+
+                if (!Argon2.Verify(user.Password, request.Password))
+                {
+                    return ResponseBase.Failed(new FieldError
+                    {
+                        Field = nameof(request.Password),
+                        Message = "Incorrect password."
+                    });
+                }
+
+                string accessToken = _jwt.CreateAccessToken(user.Id);
+
+                return ResponseBase<string>.Succeed(accessToken);
+            }
+        }
+    }
+}
---
+++ Source/Server/MediatoR/Account/Register.cs
@@ -0,0 +1,57 @@
+﻿using Isopoh.Cryptography.Argon2;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MultiplayerSnake.Server.Dtos;
+using MultiplayerSnake.Server.Entities;
+using MultiplayerSnake.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Server.MediatR.Account
+{
+    public static class Register
+    {
+        public class Handler : IRequestHandler<UserRegisterDto, ResponseBase>
+        {
+            private readonly ApplicationDbContext _context;
+
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<ResponseBase> Handle(UserRegisterDto request, CancellationToken cancellationToken)
+            {
+                string hashedPassword = Argon2.Hash(request.Password);
+
+                var user = new User
+                {
+                    Username = request.Username,
+                    Password = hashedPassword
+                };
+
+                await _context.Users.AddAsync(user);
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                } catch
+                {
+                    if(await _context.Users.FirstOrDefaultAsync(u => u.Username == request.Username) != null)
+                    {
+                        return ResponseBase.Failed(new FieldError
+                        {
+                            Field = nameof(user.Username),
+                            Message = "This username is already taken."
+                        });
+                    }
+                }
+
+                return ResponseBase.Succeed;
+            }
+        }
+    }
+}
