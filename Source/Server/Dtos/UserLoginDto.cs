d3d562ca99e9bc5f67c0c502d2d45aa68fceee6e
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:24:12 +0300

    Add dtos for AccountController

---
+++ Source/Server/Dtos/UserLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using MultiplayerSnake.Server.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Server.Dtos
+{
+    public class UserLoginDto : IRequest<ResponseBase>
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
