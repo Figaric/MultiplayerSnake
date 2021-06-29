a06c07054a5aa4af09a26ed960cb78096f86b46e
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:33:54 +0300

    Modify Startup class
    Add all needed dependencies like DbContext, Validators, MediatR, JwtManager and so on

--- Source/Server/Startup.cs
+++ Source/Server/Startup.cs
@@ -1,56 +1,72 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Npgsql;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using MultiplayerSnake.Server.Hubs;
-using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using MultiplayerSnake.Server.Behaviours;
+using MultiplayerSnake.Server.Validation;
 
 namespace MultiplayerSnake.Server
 {
     public class Startup
     {
-        private readonly IConfiguration _configuration;
-
         public Startup(IConfiguration configuration)
         {
-            _configuration = configuration;
+            Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSignalR();
             services.AddMediatR(typeof(Startup).Assembly);
+
+            services.AddDbContext<ApplicationDbContext>(builder => 
+                builder.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<JwtManager>();
+
+            services.AddControllers(cfg => cfg.Filters.Add<ResponseMappingFilter>());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MultiplayerSnake.Server", Version = "v1" });
+            });
+
             services.AddValidators();
-            services.AddTransient<UserManager>();
+
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, JwtManager jwt)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MultiplayerSnake.Server v1"));
             }
+
+            app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHub<AccountHub>("account");
-
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
