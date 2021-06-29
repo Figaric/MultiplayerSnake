53c1f8de99c74ea0294a7625d61b83a6c9711af3
Figaric <59481330+Figaric@users.noreply.github.com>
2021-06-29 23:24:43 +0300

    Add User entity

---
+++ Source/Server/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MultiplayerSnake.Server.Entities
+{
+    [Index(nameof(Username), IsUnique = true)]
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Password { get; set; }
+    }
+}
