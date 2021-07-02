5f90b416fbb47d2cdae042525b629fca38e455b1
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-02 17:29:02 +0300

    Add Player class
    Representation of the User class but as a game unit

---
+++ Source/Server/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiplayerSnake.Server
+{
+    public class Player
+    {
+        public string Id { get; set; }
+
+        public ConsoleColor Color { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
