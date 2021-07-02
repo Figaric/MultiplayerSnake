9dfb7c929d84ab810fc0baeb3cd4947ef37928f3
Figaric <59481330+Figaric@users.noreply.github.com>
2021-07-02 17:27:38 +0300

    Add PlayerManager class

---
+++ Source/Server/Services/JwtManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Server
+{
+    public class JwtManager
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public int GetPayload(string accessToken)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(accessToken);
+            var tokenS = jsonToken as JwtSecurityToken;
+
+            int userId = int.Parse(tokenS.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+            return userId;
+        }
+
+        public string CreateAccessToken(int userId)
+        {
+            var symmetricKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["AccessTokenSecret"]));
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var claims = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+            });
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claims,
+                Expires = System.DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    symmetricKey,
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+    }
+}
