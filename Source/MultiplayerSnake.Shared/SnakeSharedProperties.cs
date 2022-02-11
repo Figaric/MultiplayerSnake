efc33a74904d8a11afa31a07a42f373502b8d4e9
PavlikPromix <beriya2004@mail.ru>
2022-02-11 21:00:26 +0300

    Created shared snake properties class

---
+++ Source/MultiplayerSnake.Shared/SnakeSharedProperties.cs
@@ -0,0 +1,15 @@
+﻿namespace MultiplayerSnake.Shared;
+
+class SnakeSharedProperties
+{
+    #region Properties
+    private List<Point> SParts { get; set; }
+    private List<Point> FoodCoords { get; set; }
+    public double Speed { get; set; }
+    private int Score { get; set; }
+    public bool IsAlive { get; set; }
+    public ConsoleColor Color { get; set; }
+    private RainbowColor Rainbow;
+    #endregion
+
+}
