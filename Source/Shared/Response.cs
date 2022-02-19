92b6b0884b01b8a00fa565a93daad8b80170f107
Jigen <59481330+Figaric@users.noreply.github.com>
2022-02-19 18:45:20 +0300

    Move response objects to the shared library

---
+++ Source/Shared/Response.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace MultiplayerSnake.Shared
+{
+    public interface IResponse
+    {
+        HttpStatusCode StatusCode { get; }
+
+        static IResponse Succeed(HttpStatusCode statusCode = HttpStatusCode.OK)
+            => throw new NotImplementedException();
+    }
+
+    public class ResponseBase : IResponse
+    {
+        public HttpStatusCode StatusCode { get; protected set; }
+
+        public static IResponse Succeed(HttpStatusCode statusCode = HttpStatusCode.OK)
+            => new ResponseBase { StatusCode = statusCode };
+    }
+
+    public class ResponseFail<TError> : ResponseBase, IResponse
+    {
+        public IEnumerable<TError> Errors { get; private set; }
+
+        public static IResponse Failed(HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+            => new ResponseFail<TError> { StatusCode = statusCode };
+
+        public static IResponse Failed(TError error, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+            => new ResponseFail<TError> { Errors = new List<TError> { error }, StatusCode = statusCode };
+
+        public static IResponse Failed(IEnumerable<TError> errors, HttpStatusCode statusCode = HttpStatusCode.BadRequest)
+            => new ResponseFail<TError> { Errors = errors, StatusCode = statusCode };
+    }
+
+    public class ResponseData<TData> : ResponseBase, IResponse
+    {
+        public TData Data { get; private set; }
+
+        public static IResponse Succeed(TData data, HttpStatusCode statusCode = HttpStatusCode.OK)
+            => new ResponseData<TData> { Data = data, StatusCode = statusCode };
+    }
+}
