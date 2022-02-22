bfe9c46b5c67d8259e7f0e83d6eda8e05ac1cbaf
PavlikPromix <beriya2004@mail.ru>
2022-02-22 17:49:41 +0300

    Created class to manage SignalR connection

---
+++ Source/Client/NetSession.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Client
+{
+    class NetSession
+    {
+        public Account UserAccount { get; set; }
+        public HubConnection Connection { get; private set; }
+
+        public NetSession(Account userAccount)
+        {
+            UserAccount = userAccount;
+        }
+
+        public async Task Connect()
+        {
+            if (UserAccount.Logon)
+            {
+                Connection = new HubConnectionBuilder()
+                    .WithUrl($"http://localhost:5000/{87}", options =>
+                    {
+                        options.AccessTokenProvider = () => Task.FromResult(UserAccount.JwtToken);
+                    }).Build();
+
+                await Connection.StartAsync();
+            }
+        }
+    }
+}
