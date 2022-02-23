57a622ac7091aee8f3b663f97518e0b61373833f
PavlikPromix <beriya2004@mail.ru>
2022-02-23 16:51:47 +0300

    Created two response prototypes

---
+++ Source/Client/ResponsePrototypes/ResponseError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Client
+{
+    internal class SingleError
+    {
+        public string Field { get; set; }
+        public string Message { get; set; }
+    }
+    class ResponseError
+    {
+        public List<SingleError> Errors { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
---
+++ Source/Client/ResponsePrototypes/ResponseSuccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Client
+{
+    internal class Data
+    {
+        public string JwtToken { get; set; }
+    }
+    class ResponseSuccess
+    {
+        public Data Data { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
