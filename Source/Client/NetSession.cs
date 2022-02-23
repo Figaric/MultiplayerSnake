375870bc6afd586575ef3a133ab7768b554965aa
PavlikPromix <beriya2004@mail.ru>
2022-02-23 16:54:47 +0300

    Prepared SignalR connection methods

--- Source/Client/NetSession.cs
+++ Source/Client/NetSession.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultiplayerSnake.Client
@@ -21,14 +19,40 @@ namespace MultiplayerSnake.Client
         {
             if (UserAccount.Logon)
             {
+                Console.Clear();
+                Console.WriteLine("\n\tПодключение к серверу...");
                 Connection = new HubConnectionBuilder()
-                    .WithUrl($"http://localhost:5000/{87}", options =>
+                    .WithUrl("http://localhost:5000/hubs/gamehub", options =>
                     {
                         options.AccessTokenProvider = () => Task.FromResult(UserAccount.JwtToken);
                     }).Build();
 
                 await Connection.StartAsync();
+
+                while (Connection.State != HubConnectionState.Disconnected)
+                {
+                    if (Connection.State == HubConnectionState.Connected)
+                    {
+                        Console.WriteLine("\n\t\tУспешно подключен!");
+                        Thread.Sleep(200);
+                        return;
+                    }
+                }
+                Console.WriteLine("\n\t\tНе удаётся подключится к серверу!");
+                Thread.Sleep(500);
+                return;
             }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("\n\tВы не вошли в аккаунт");
+                Thread.Sleep(500);
+            }
+        }
+
+        public async Task HostLobby()
+        {
+            await Connection.InvokeAsync("CreateRoom");
         }
     }
 }
