81c5e87c2a89808e71ff88d19ed52f6bc8e0b216
Jigen <59481330+Figaric@users.noreply.github.com>
2022-02-24 17:54:24 +0300

    Created file that keeps all of the hub method names

---
+++ Source/Shared/HubMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplayerSnake.Shared
+{
+    public static class HubMethods
+    {
+        public const string CreateRoom = "CreateRoom";
+
+        public const string JoinRoom = "JoinRoom";
+
+        public const string RoomCreated = "RoomCreated";
+
+        public const string RoomJoined = "RoomJoined";
+
+        public const string GetRooms = "GetRooms";
+
+        public const string RoomsReceived = "RoomsReceived";
+    }
+}
